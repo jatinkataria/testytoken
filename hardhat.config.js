b433602b76439eb09f8e47ab3095f68eddceb404
jk <jk@jennifers-mbp.lan>
2021-10-17 14:22:32 -0400

    adding transfer script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,12 +11,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+
+  namedAccounts: {
+    deployer: 0,
+  },
+  paths: {
+    sources: 'contracts',
+  },
+
 };
