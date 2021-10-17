6a111fabb6b8b6855b2120c627008257cb9daa75
jk <jk@jennifers-mbp.lan>
2021-10-17 13:30:04 -0400

    test contract test and deployment script

---
+++ scripts/test-token-deploy.js
@@ -0,0 +1,23 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+
+  const Testy = await hre.ethers.getContractFactory("MyTestContract");
+  const testChain = await Testy.deploy();
+
+  await testChain.deployed();
+
+  console.log("Test Chain deployed to:", testChain.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
